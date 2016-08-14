2bd34b28163e12c82a39bb1635535620c695f638
wzwmxd <wzwmxd@qq.com>
2016-08-14 09:29:22 +0800

    2016/8/14

--- Hack/my_test.py
+++
@@ -1,5 +0,0 @@
-import my_debugger
-
-debugger = my_debugger.debugger()
-
-debugger.load("C:\\Windows\\system32\\calc.exe")
--- Python高级编程/iter_1.py
+++
@@ -1,5 +0,0 @@
-import tokenize
-
-reader = open('./amina.py').next()
-tokens = tokenize.generate_tokens(reader)
-print(tokens.next())
